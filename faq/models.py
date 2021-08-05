26690870d6a294685939b52bc961640de304ff19
suping106 <lou_suping@yahoo.com>
2021-08-05 18:14:26 +0000

    setup deployment and updated model.py in faq

--- faq/models.py
+++ faq/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class FAQ(models.Model):
     name = models.CharField(max_length=254)
     question = models.CharField(max_length=254, null=False, blank=False)
-    answer = models.CharField(max_length=254, null=False, blank=False)
+    answer = models.CharField(max_length=1000, null=False, blank=False)
 
     def __str__(self):
         return self.name
